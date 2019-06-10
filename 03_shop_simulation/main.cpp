9ec3003d50c62928693959d16b1a4fdefda9a951
Paweł Kobryński <pawelkob97@wp.pl>
2019-06-10 23:19:44 +0200

    03_initial_commit

---
+++ 03_shop_simulation/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <ncurses.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[  ])
+{
+    std::cout<<"DOBRA ROBOTA PAWEŁ DZIEŃ PRZED ODDANIEM ZACZYNAĆ PROJEKT GG WP GL HF"<<std::endl;
+    return 0;
+}
