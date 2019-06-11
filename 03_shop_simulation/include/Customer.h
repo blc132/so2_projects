61298eb3ab707803d5ad36304e2a92855f08f006
Paweł Kobryński <pawelkob97@wp.pl>
2019-06-11 03:42:22 +0200

    Dodanie szkieletu klasy Customer

---
+++ 03_shop_simulation/include/Customer.h
@@ -0,0 +1,23 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <ncurses.h>
+#include <vector>
+#include <mutex>
+
+#include "Data.h"
+
+class Customer
+{
+public:
+
+    Customer();
+    ~Customer();
+private:
+
+};
+
+#endif
